7d070e9d863230fe2046874196b34866cf272517
Jun Hendrex Escarrilla <junhendrex.escarrilla@gsc.edu.ph>
2023-02-13 16:25:12 +0800

    Login page

---
+++ include/style.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+<style type="text/css">
+*{
+	box-sizing: border-box;
+}	
+.container_login{
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+
+	height: 100vh;
+	width: 100%;
+}
+.card_container{
+	width: 400px;
+}
+.card_container label{
+	font-weight: 500;
+}
+</style>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Login | OJT Information System</title>
+	<?php include('include/style.php'); ?>
+</head>
+<body>
+
+<div class="container_login">
+	<div class="card card_container">
+	  	<div class="card-body px-5 py-4">
+	    	<h1 class="card-title text-center mb-3">LOGIN</h1>
+	  		<div class="form-group">
+	  			<label>EMAIL OR USERNAME</label>
+	  			<input type="text" name="" class="form-control">
+	  		</div>
+	  		<div class="form-group">
+	  			<label>PASSWORD</label>
+	  			<input type="password" name="" class="form-control">
+	  		</div>
+	  		<div class="form-group">
+	  			<button class="btn btn-primary btn-block btn-lg"><strong>LOGIN</strong></button>
+	  		</div>
+  		</div>
+	</div>
+</div>
+
+<?php include('include/script.php'); ?>
+</body>
+</html>
