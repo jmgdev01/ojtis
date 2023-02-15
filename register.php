55aa27455c693d72ad16631951a2591eb4949060
Kem S. Gallo <kemgallogithub213@gmail.com>
2023-02-15 16:39:09 +0300

    Modify the register page specifically in the tabs icon and change the color of personal information.

--- register.php
+++ register.php
@@ -2,7 +2,7 @@
 
 include('include/config.php');
 
-if (isset($_POST['btn_submit'])) {
+if (isset($_POST['btn_register'])) {
 
 	$firstname = $_POST['i_fname'];
 	$middlename = $_POST['i_mname'];
@@ -98,7 +98,7 @@ if (isset($_POST['btn_submit'])) {
 				<h1 class="card_header">CREATE AN ACCOUNT</h1>
 			</div>
 	  		<!-- personal info -->
-			<ul class="nav nav-tabs" id="myTab" role="tablist">
+			<ul class="nav nav-tabs d-flex align-items-center justify-content-center" id="myTab" role="tablist">
 				<li class="nav-item" role="presentation">
 					<button 
 					class="nav-link active" 
@@ -173,7 +173,7 @@ if (isset($_POST['btn_submit'])) {
 				<div class="tab-pane fade show active" id="personal_info" role="tabpanel">
 					
 				<div class="row">
-					<div class="form-group col-lg-12 pt-4">
+					<div class="form-group col-lg-12 pt-4" style="color: #FFBF00;">
 						<h3><strong>PERSONAL INFORMATION</strong></h3>
 					</div>
 					<div class="form-group col-lg-4 col-md-7">
@@ -247,77 +247,6 @@ if (isset($_POST['btn_submit'])) {
 						<input type="number" name="i_weight" class="form-control" placeholder="- kg -" min="1">
 					</div>
 				</div>
-<<<<<<< HEAD
-				<div class="form-group col-lg-3 col-md-5">
-					<label><small><strong>MIDDLE NAME</strong></small></label>
-					<input type="text" name="i_mname" class="form-control" placeholder="e.g. Garcia">
-				</div>
-				<div class="form-group col-lg-3 col-md-7">
-					<label><small><strong>LAST NAME</strong></small></label>
-					<input type="text" name="i_lname" class="form-control" placeholder="e.g. Cruz">
-				</div>
-				<div class="form-group col-lg-2 col-md-5">
-					<label><small><strong>EXT.</strong></small></label>
-					<select class="form-control" name="i_extname">
-						<option value="">- Select -</option>
-						<option value="Jr.">Jr.</option>
-						<option value="Sr.">Sr.</option>
-						<option value="III">III</option>
-						<option value="IV">IV</option>
-						<option value="V">V</option>
-					</select>
-				</div>
-				<div class="form-group col-lg-5 col-md-5">
-					<label><small><strong>DATE OF BIRTH</strong></small></label>
-					<input type="date" name="i_bdate" class="form-control" placeholder="">
-				</div>
-				<div class="form-group col-lg-3 col-md-3">
-					<label><small><strong>AGE</strong></small></label>
-					<input type="number" name="i_age" class="form-control" placeholder="">
-				</div>
-				<div class="form-group col-lg-4 col-md-4">
-					<label><small><strong>SEX</strong></small></label>
-					<select class="form-control" name="i_sex">
-						<option value="">- Select -</option>
-						<option value="Male">Male</option>
-						<option value="Female">Female</option>
-					</select>
-				</div>
-				<div class="form-group col-lg-3 col-md-3">
-					<label><small><strong>STATUS</strong></small></label>
-					<select class="form-control" name="i_cstatus">
-						<option value="">- Select -</option>
-						<option value="Single">Single</option>
-						<option value="Married">Married</option>
-						<option value="Widow">Widow</option>
-					</select>
-				</div>
-				<div class="form-group col-lg-3	 col-md-3">
-					<label><small><strong>BLOODTYPE</strong></small></label>
-					<select class="form-control" name="i_btype">
-						<option value="">- Select -</option>
-						<option value="A+">A+</option>
-						<option value="A-">A-</option>
-						<option value="B+">B+</option>
-						<option value="B-">B-</option>
-						<option value="O+">O+</option>
-						<option value="O-">O-</option>
-						<option value="AB+">AB+</option>
-						<option value="AB-">AB-</option>
-					</select>
-				</div>
-				<div class="form-group col-lg-3 col-md-3">
-					<label><small><strong>HEIGHT</strong></small></label>
-					<input type="number" name="i_height" class="form-control" placeholder="- cm -" min="1">
-				</div>
-				<div class="form-group col-lg-3 col-md-3">
-					<label><small><strong>WEIGHT</strong></small></label>
-					<input type="number" name="i_weight" class="form-control" placeholder="- kgs -" min="1">
-				</div>
-				
-=======
->>>>>>> 87d27af07a3f70f186b2d12174c4f84fbcd39a7a
-
 				</div>
 				<div class="tab-pane fade" id="address_contact_info" role="tabpanel">
 					
@@ -326,7 +255,7 @@ if (isset($_POST['btn_submit'])) {
 
 			<div class="col-lg-12">
 				<div class="form-group col-lg-12 text-center">
-					<button class="form-btn form-btn-md btn-blue"><strong>REGISTER</strong></button>
+					<button class="form-btn form-btn-md btn-blue" name="btn_register"><strong>REGISTER</strong></button>
 				</div>
 			</div>
 
