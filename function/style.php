e28bdaca5788b6c987cc48d920cda2c21d483040
jmgdev01 <jmg0125.github@gmail.com>
2023-02-15 22:12:51 +0800

    Updated the page logo

--- function/style.php
+++ function/style.php
@@ -1,4 +1,4 @@
-<link rel="icon" type="image/x-icon" href="assets/logo/logo-small.png">
+<link rel="icon" type="image/x-icon" href="assets/logo/logo-small2.png">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 <link rel="stylesheet" href="assets/css/font-awesome/css/font-awesome.min.css">
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
