f273859b02b98d90f1fc15c4d6ac4162fd419bd3
ESCARRILLA <junhendrex.escarrilla@gsc.edu.ph>
2023-02-22 14:35:26 +0800

    added UI in admin profile and account as well as functionality in account section

--- users/admin/include/style.php
+++ users/admin/include/style.php
@@ -76,8 +76,8 @@ href="../../assets/css/font-awesome/css/font-awesome.min.css">
 /* Alert */
 	#msg_alert {
 		position: fixed;
-		z-index: 100;
-		top: 30px;
+		z-index: 2000;
+		top: 80px;
 		right: 30px;
 
 		width: 350px;
