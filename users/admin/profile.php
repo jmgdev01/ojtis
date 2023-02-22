9581fce76dad969e03ee72010a408f75641ce1d5
Kem S. Gallo <kemgallogithub213@gmail.com>
2023-02-22 17:09:34 +0300

    Create a Functionality that store the picture in the file manager folder

--- users/admin/profile.php
+++ users/admin/profile.php
@@ -18,6 +18,14 @@
                 </button>
             </div>";
         } else {
+
+            if(isset($_FILES['image'])){
+                $file_name = $_FILES['image']['name'];
+                $file_tmp = $_FILES['image']['tmp_name'];
+                move_uploaded_file($file_tmp, "../../assets/profile/" . $file_name);
+                echo "Stored in: " . "../../assets/profile/" . $_FILES['image']['name'];
+            }
+
             $sql1 = mysqli_query($db, "UPDATE admin_tbl SET a_fullname='$a_fullname', a_mobile='$a_mobile' WHERE a_id='$a_id'");
 
             echo "<div id='msg_alert' class='alert bg-success alert-dismissible fade show' role='alert'>
@@ -31,6 +39,7 @@
 
     $sql = mysqli_query($db, "SELECT * FROM admin_tbl INNER JOIN accounts_tbl ON admin_tbl.a_id = accounts_tbl.a_id WHERE accounts_tbl.acc_id='$acc_id'");
     $res = mysqli_fetch_assoc($sql);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -48,7 +57,7 @@
     <?php include("include/nav.php"); ?>
     
     <div class="container py-5">
-            <form method="post" action="profile.php">
+            <form method="POST" action="profile.php" enctype="multipart/form-data">
                 <input type="hidden" name="a_id" value="<?php echo $res['a_id']; ?>">
                 <div class="row d-flex justify-content-center">
                     <div class="col-lg-10 col-md-12">
@@ -58,15 +67,13 @@
                                 <div class="row">
                                     <div class="col-lg-6 col-md-12 col-sm-12">
                                         <div class="row">
-                                            <form action="profile.php" method="post">
-                                                <div class="col-md-12 col-md-12 pb-3">
-                                                    <label><small><strong>Profile Image</strong></small></label>
-                                                    <div class="col-lg-12 col-md-12 col-sm-12" style="width: 100%; height: 200px; background-color: #ccc; margin-bottom: 10px; text-align: center; padding: 20px;">
-                                                        <img id="image_preview" src="#" alt="your image" style="display: none; max-width: 100%; max-height: 100%;">
-                                                    </div>
-                                                    <input type="file" name="profile_image" id="profile_image">
+                                            <div class="col-md-12 col-md-12 pb-3">
+                                                <label><small><strong>Profile Image</strong></small></label>
+                                                <div class="col-lg-12 col-md-12 col-sm-12" style="width: 100%; height: 200px; background-color: #ccc; margin-bottom: 10px; text-align: center; padding: 20px;">
+                                                    <img id="image_preview" src="#" alt="your image" style="display: none; max-width: 100%; max-height: 100%;">
                                                 </div>
-                                            </form>
+                                                <input type="file" name="image" id="image">
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="col-lg-6">
@@ -75,16 +82,16 @@
                                                 <label><small><strong>FULL NAME <span class="text-danger">*</span></strong></small></label>
                                                 <input type="text" id="a_fullname" name="a_fullname" class="form-control" placeholder="e.g. Juan Dela Cru" value="<?php echo $res['a_fullname']; ?>">
                                             </div>
-                                            <div class="form-group col-lg-6 col-md-12">
+                                            <div class="form-group col-lg-12 col-md-12">
                                                 <label><small><strong>CONTACT NO<span class="text-danger">*</span></strong></small></label>
-                                                <input type="text" id="s_mobile_no" name="s_mobile_no" class="form-control" placeholder="e.g. 09123456789" value="">
+                                                <input type="text" id="s_mobile_no" name="a_mobile" class="form-control" placeholder="e.g. 09123456789" value="<?php echo $res['a_mobile']; ?>">
                                             </div>
                                             <div class="form-group col-lg-12 text-center">
                                                 <button class="form-btn form-btn-md btn-blue" type="submit" id="btn_save" name="btn_save"><strong><i class="fa fa-floppy-o" aria-hidden="true"></i> Save</strong></button>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
+                                </div>  
                             </div>
                         </div>    
                     </div>
